f2adc20cb4dfd2749e00a35362537bcbb5212ae8
santhosh <santhoshkoppisetti89@gmail.com>
2026-04-04 15:56:20 +0530

    static

--- Main_Project/settings.py
+++ Main_Project/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -140,3 +141,6 @@ LOGIN_URL = '/login/'
 
 # After successful login, send them to dashboard
 LOGIN_REDIRECT_URL = '/dashboard/'
+
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
